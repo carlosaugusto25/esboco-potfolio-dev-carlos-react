61524b25189639340a019e118265ac7eded7f03a
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-11-06 21:09:26 -0300

    correção card project

--- src/components/CardProject/index.tsx
+++ src/components/CardProject/index.tsx
@@ -17,7 +17,7 @@ export function CardProject({ id, title, description, tech, path }: CardProps) {
         <a href={path} target="_blank" rel="noreferrer" style={{textDecoration: 'none', color: 'black'}}>
         <div className={styles.container}>
             <div className={styles.imgProject}>
-                <img src={id === 1 ? starwars : id === 2 ? primeflix : id === 3 ? tarefas : architecture} alt="" />
+                <img src={id === 1 ? starwars : id === 2 ? primeflix : id === 3 ? tarefas : id === 4 ? architecture : architecture} alt="" />
                 <div className={styles.technologies}>
                     {tech?.map((tech) => (
                             <div className={styles.content}>{tech}</div>
